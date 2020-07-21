4ae1795b56a4d3f3f4707872246923b375e93641
kazuki <kitatanabe0709@gmail.com>
2020-07-21 03:58:16 +0000

    follow, unfollow

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,32 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color :#0066FF; height :80px;">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">P_share</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    {{-- ユーザ一覧ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('users.index', 'ユーザー一覧', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ詳細ページへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('users.show', 'マイプロフィール', ['user' => Auth::id()]) !!}</li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                        </ul>
+                    </li>
+                
+                @endif
+            </ul>
+        </div>
+    </nav>
+</header>
+  
