387102cf4134389fe948f28ab10cf62048a463b8
kazuki <kitatanabe0709@gmail.com>
2020-08-05 09:35:32 +0000

    resolve #1

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,12 +1,12 @@
 @if (Auth::user()->is_favorite($post->id))
 {{-- お気に入り解除ボタンのフォーム --}}
 {!! Form::open(['route' => ['favorites.unfavorite', $post->id], 'method' => 'delete']) !!}
-{!! Form::submit('Unfavorite', ['class' => "btn btn-dark btn-sm"]) !!}
+<button type="submit" class="btn" style="font-size: 30px; color: blue;"><i class="fas fa-heart-broken"></i></button>
 {!! Form::close() !!}
 @else
 {{-- お気に入りボタンのフォーム --}}
 {!! Form::open(['route' => ['favorites.favorite', $post->id]]) !!}
-<button type="submit" class="btn" style="font-size: 30px;"><i class="fas fa-crown"></i></button>
+<button type="submit" class="btn" style="font-size: 30px; color: red;"><i class="fas fa-heart"></i></button>
 {!! Form::close() !!}
 @endif
 
